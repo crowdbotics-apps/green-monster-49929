ab267b37a23c56ee5f0f1780e8761f0620b1f185
Crowdbotics <team@crowdbotics.com>
2024-11-07 07:56:49 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "green-monster-49929",
+  url: "https://green-monster-49929.botics.co",
+  api: "https://green-monster-49929.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
